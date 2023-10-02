525a462235a8f9f91011d10a6de0e4667dcbf209
UlfrJarl <luchik.samara@yandex.ru>
2023-10-03 01:09:48 +0400

    12

--- src/coffee/CoffeeMachine.java
+++ src/coffee/CoffeeMachine.java
@@ -1,7 +1,11 @@
 package coffee;
 
-public class Cappuccino {
+public class CoffeeMachine {
     public void makeCappuccino(){
         System.out.println("Вот твой капучино");
+    }
+
+    public void makeLatte(){
+        System.out.println("Вот твой Латте");
     }
 }
--- src/coffee/Main.java
+++ src/coffee/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
 Title title = new Title();
 title.ShowTitle();
-Cappuccino cappuccino = new Cappuccino();
-cappuccino.makeCappuccino();
+
+CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+coffeeMachine.makeCappuccino();
+coffeeMachine.makeLatte();
     }
 }
