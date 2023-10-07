da305ee64f7fa19b549507bf885f4a530bba4fe7
UlfrJarl <luchik.samara@yandex.ru>
2023-10-07 18:19:00 +0400

    12

--- src/recipe/Espresso.java
+++ src/recipe/Espresso.java
@@ -7,7 +7,7 @@ import static Ingredient.CoffeePowderGramm.*;
 public class Espresso {
     public CoffeePowderGramm addEspresso(CoffeePowderGramm coffeePowderGramm) {
 
-        System.out.println("Your " + String.valueOf(coffeePowderGramm) +" espresso");
+        System.out.println("Your " + coffeePowderGramm +" espresso");
         return valueOf(String.valueOf(coffeePowderGramm));
     }
 }
