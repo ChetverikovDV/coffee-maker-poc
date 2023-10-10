84ed9e72775efaa249a1c4eaf5fe43f251c3ef64
DMITRY CHETVERIKOV <76950379+ChetverikovDV@users.noreply.github.com>
2023-10-11 02:04:22 +0400

    Спать пора

--- src/coffee/Coffee.java
+++ src/coffee/Coffee.java
@@ -1,12 +1,5 @@
 package coffee;
 
-import Ingredient.CoffeeSort;
-
 public class Coffee {
-    //  String sort = String.valueOf(CoffeeSort.Mokka);
-
-   // public void getSort() {
-   //     System.out.println(sort);
-   // }
 
 }
