0a15371cf01d08bfc297dc03b74c937282b7014e
DMITRY CHETVERIKOV <76950379+ChetverikovDV@users.noreply.github.com>
2023-10-24 23:32:37 +0400

    Спать пора

--- src/module/Boiler.java
+++ src/module/Boiler.java
@@ -1,11 +1,8 @@
 package module;
 
 import hardware.CurrentRecipe;
-import ingredient.Grinding;
 
 public class Boiler {
-
-    Grinding grinding;
 
     public void boil(CurrentRecipe coffee) {
         System.out.println(
